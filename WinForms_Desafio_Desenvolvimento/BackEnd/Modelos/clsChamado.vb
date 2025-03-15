51fb3864cded5f42519fa4c1224215301b1d8c65
Felipe Henrique C Machado <FelipeSI2015@outlook.com.br>
2025-03-14 21:36:16 -0300

    Creation of the classes Departamento, Chamado, ValidacaoDepartamento, and ValidacaoChamados; Creation of the Validation methods;

---
+++ WinForms_Desafio_Desenvolvimento/BackEnd/Modelos/clsChamado.vb
@@ -0,0 +1,14 @@
+﻿Public Class Chamado
+
+    Public Property Id As Integer
+    Public Property Assunto As String
+    Public Property Solicitante As String
+    Public Property DataAbertura As DateTime
+
+    Public Sub New(assunto As String, solicitante As String, dataAbertura As DateTime)
+        Me.Assunto = assunto
+        Me.Solicitante = solicitante
+        Me.DataAbertura = dataAbertura
+    End Sub
+
+End Class
--- WinForms_Desafio_Desenvolvimento/frmDepartamentosEditar.vb
+++ WinForms_Desafio_Desenvolvimento/frmDepartamentosEditar.vb
@@ -22,24 +22,36 @@ Public Class frmDepartamentosEditar
 
         If Not String.IsNullOrEmpty(Me.txtID.Text) Then
             ID = Integer.Parse(Me.txtID.Text)
-
         End If
 
-        Dim sucesso As Boolean = DadosDepartamentos.GravarDepartamento(ID, Descricao)
+        Dim departamento As New Departamento(descricao)
 
-        If Not sucesso Then
+        If Not ValidarDepartamento(departamento) Then
+            Exit Sub
+        End If
 
-            MessageBox.Show(Me, "Falha ao gravar o departamento", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Me.DialogResult = DialogResult.Cancel
+        Dim sucesso As Boolean = DadosDepartamentos.GravarDepartamento(ID, descricao)
 
-        Else
-
+        If sucesso Then
             Me.DialogResult = DialogResult.OK
-
+        Else
+            MessageBox.Show(Me, $"Falha ao gravar o departamento", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.DialogResult = DialogResult.Cancel
         End If
 
         Me.Close()
 
     End Sub
+
+    Private Function ValidarDepartamento(departamento As Departamento) As Boolean
+
+        Dim erros = ValidacaoDepartamento.Validar(departamento)
+
+        If erros.Any() Then
+            MessageBox.Show(String.Join(vbCrLf, erros), "Erro de Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return False
+        End If
+        Return True
+    End Function
 
 End Class
